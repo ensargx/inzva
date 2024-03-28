0c5fdeb2b7877610f891f295b01f9197eabb0364
Ensar Gök <ensarxgok@gmail.com>
2024-03-28 20:47:59 +0300

    yasin

---
+++ yasin.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    long long n;
+    std::cin >> n;
+    n++;
+    long long ans = n;
+
+    while ((n=n-2) > 2)
+    {
+        // n-=2;
+        // if (n<3)
+        //     break;
+        long long tmp = n / 3;
+        ans -= 2*tmp;
+        n = tmp;
+    }
+    std::cout << ans - 1 << '\n';
+
+    return 0;
+}
